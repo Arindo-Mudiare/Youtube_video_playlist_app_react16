e0c7bd6ddef49edfa81894c22baf5c105a062ca8
Arindo <ArindoTmudiare@gmail.com>
2018-11-03 01:53:10 +0100

    Introduced the youtube video search API in the parent component and converted it to a class based component as well

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -1,15 +1,23 @@
 import React, { Component } from 'react';
 
-// stateless component
-
-// const SearchBar = () => {
-//   return <input />;
-// };
-
 // class based component
 class SearchBar extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      term: ''
+    };
+  }
   render() {
-    return <input />;
+    return (
+    <div>
+      <br></br>
+      <input onChange={(event) => this.setState({ term: event.target.value}) }/>
+      <br></br>
+      Value of the input: {this.state.term}
+    </div> 
+    );
   }
 }
 
--- src/index.js
+++ src/index.js
@@ -1,9 +1,26 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
+import YouTubeSearch from 'youtube-api-search';
 import SearchBar from './components/search_bar';
-
 const API_KEY = 'AIzaSyBzVVlu4cUX88aoJWs0Pi9TfCvfOMRMNxs';
-const App = () => <div> <SearchBar /> </div>;
+
+
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = { videos: []};
+    YouTubeSearch({key:API_KEY, term: 'Anthony Joshua boxing'}, (videos) => this.setState({ videos }))
+  }
+  render() {
+    return (
+      <div> 
+        <SearchBar /> 
+      </div>
+    );
+  }
+} 
 
 ReactDOM.render(<App />,document.querySelector('.container'));
 
